f6e1ffa70d4001319c7e29c79e7a11137f3ee7c7
main <https://github.com/alexandre3232/CalculadoraSimples.git>
2023-04-12 12:55:37 -0300

    add comentarios ao script.js

--- script/script.js
+++ script/script.js
@@ -1,17 +1,22 @@
+
+//função para inserir os números no display da calculadora.
 function insert(valor){
   let numero = document.querySelector('#display').innerHTML;
     document.querySelector('#display').innerHTML = numero + valor;
 }
 
+//função para apaga tudo no display da calculadora.
 function clean(){
   document.querySelector('#display').innerHTML = '';
 }
 
+//função para apara o ultimo digito no display da calculadora.
 function apaga(){
   let valor = document.querySelector('#display').innerHTML;
   document.querySelector('#display').innerHTML = valor.substring(0, valor.length -1);
 }
 
+//função que realiza a operação de calcular.
 function calcular(){
   let resultado = document.querySelector('#display').innerHTML;
   if(resultado){
